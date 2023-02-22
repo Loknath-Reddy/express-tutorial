0d79841237934639820f33abe289184dca241ab7
Loknath Reddy <loknathreddy11082001@gmail.com>
2023-02-22 23:36:52 +0530

    Delete msg

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
